b55b351503d77225ee45f173eee50cee8c4f54ff
boombard <emerton222@gmail.com>
2016-05-19 11:48:12 +0100

    added dependencies to setup.py

--- setup.py
+++ setup.py
@@ -5,5 +5,16 @@ from setuptools import setup
 setup(
     name='scrnatb',
     version='0.0',
-    packages=['scrnatb']
+    packages=['scrnatb'],
+    install_requires=[
+        'GPy',
+        'seaborn',
+        'scipy',
+        'tqdm',
+        'matplotlib',
+        'statsmodels',
+        'patsy',
+        'GPclust'
+    ],
+    dependency_links=['https://github.com/SheffieldML/GPclust/tarball/master#egg=GPclust-0.1.0']
 )
